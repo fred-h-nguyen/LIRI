c2098eed65ab4b48d1af60674694ce49af2c1008
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-08-13 21:39:12 -0500

    get all variables and packages set

---
+++ liri.js
@@ -0,0 +1,31 @@
+// all required packages
+//axios used for omdb and bands api
+var axios = require('axios');
+
+// used to pull spotify keys from keys.js
+require('dotenv').config();
+var Spotify = require('node-spotify-api');
+var keys = require('./keys.js');
+var spotify = new Spotify(keys.spotify);
+
+//moment used to get time for concerts
+var moment = require('moment');
+
+// process.argv to get commands
+var args = process.argv;
+
+function concert(artist) {
+
+    axios.get('https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp')
+        .then(function (response) {
+
+            var data = response.data
+
+
+
+        })
+
+
+}
+
+
